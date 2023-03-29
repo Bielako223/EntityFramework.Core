5c115509b3cfd13a24177508ca57c2aef09032b6
Mateusz bielaszewski <mateusz-bielaszewski@o2.pl>
2023-03-29 23:55:39 +0200

    Working EF

---
+++ EFDataAccessLibrary/DataAccess/PeopleContext.cs
@@ -0,0 +1,15 @@
+﻿using EFDataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace asp_net_ef_test.DataAccess
+{
+    public class PeopleContext: DbContext
+    {
+        public PeopleContext(DbContextOptions options) : base(options) { }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Email> EmailAddresses { get; set; }
+
+
+    }
+}
---
+++ asp net ef test/Pages/Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using asp_net_ef_test.DataAccess;
+using EFDataAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace asp_net_ef_test.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly PeopleContext _db;
+
+        public IndexModel(ILogger<IndexModel> logger,  PeopleContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            LoadSampleData();
+            var people = _db.People
+                .Include(x => x.Addresses)
+                .Include(x => x.EmailAdresses)
+                .ToList();
+        }
+
+        public void LoadSampleData()
+        {
+            if (_db.People.Count()==0)
+            {
+                string file = System.IO.File.ReadAllText("generated.json");
+                var people = JsonSerializer.Deserialize<List<Person>>(file);
+                _db.AddRange(people);
+                _db.SaveChanges();
+            }
+        }
+    }
+}
